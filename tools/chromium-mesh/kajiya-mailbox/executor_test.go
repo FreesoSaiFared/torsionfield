989c8ed58d776905f1293a49332978ecc15c98b0
Gerson <intelligible.design.blog@gmail.com>
2026-08-15 15:22:46 +0200

    test exact Kajiya mailbox executor fields

--- tools/chromium-mesh/kajiya-mailbox/executor_test.go
+++ tools/chromium-mesh/kajiya-mailbox/executor_test.go
@@ -19,11 +19,10 @@ func TestExecutorRoundTrip(t *testing.T) {
 	tr := &fakeTransport{}
 	ex := &Executor{Transport: tr}
 	a := &model.Action{
-		Args:        []string{"tool", "--flag"},
-		EnvVars:     []model.EnvVar{{Name: "TF_TEST", Value: "yes"}},
-		WorkingDir:  "out/Default",
-		OutputPaths: []string{"obj/a.o"},
-		Platform:    map[string][]string{"OSFamily": {"Linux"}},
+		Args:       []string{"tool", "--flag"},
+		EnvVars:    []model.EnvVar{{Name: "TF_TEST", Value: "yes"}},
+		WorkingDir: "out/Default",
+		Platform:   map[string][]string{"OSFamily": {"Linux"}},
 	}
 
 	r, err := ex.Execute(a)
